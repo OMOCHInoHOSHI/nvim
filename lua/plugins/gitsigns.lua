01a95866caad77496ec6a41262a89a05c46e7446
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-01-17 21:56:59 +0900

    誰がコミットしたかわかるようにした

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,15 +1,22 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "-" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    })
-  end,
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			-- これを有効にすると、カーソル行に情報が出ます
+			current_line_blame = true,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = 'eol', -- 行末に表示
+				delay = 000,
+			},
+			-- 変更が分かりやすく
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "-" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		})
+	end,
 }
-
