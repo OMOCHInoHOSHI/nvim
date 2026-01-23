80a2b0a73609d7f77355c46d96851a5773445f93
OMOCHInoHOSHI <ittamago2020kai@gmail.com>
2026-01-24 00:47:46 +0900

    render-markdownを導入

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- あなたの環境にあるアイコンプラグインを指定
+    dependencies = { 
+        'nvim-treesitter/nvim-treesitter', 
+        'nvim-tree/nvim-web-devicons' 
+    },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
